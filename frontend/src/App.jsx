a3d20e3f0c11d8a50e70039cec53389f24bc2bf7
marcopoltergeist <paul_dubor@outlook.fr>
2024-02-01 22:52:00 +0100

    add navigation add image-slider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
+import { useEffect } from "react";
 import './styles/App.css'
 import {Outlet} from "react-router-dom";
 import Header from "./components/Header.jsx";
+import ImageSlider from "./components/ImageSlider.jsx";
 
 function App() {
   return (
     <>
       <Header/>
       <main>
+      <ImageSlider />
         <Outlet/>
       </main>
     </>
